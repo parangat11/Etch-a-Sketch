7e77e3d2a68d8b531ed66612c26236adab9c0b11
parangat11 <parangataditya001@gmail.com>
2024-09-29 17:14:16 +0530

    Add style to the squares generated to get a better visibility.
    The canvas is now conspicuous.

--- script.js
+++ script.js
@@ -7,14 +7,22 @@ console.log(dimension);
 
 // Creating the canvas
 const container = document.querySelector(".container");
+const height = 960 / dimension;
+const width = height;
 
 for(let col = 0; col < dimension; col++) {
     const div = document.createElement("div");
     container.appendChild(div);
-    div.classList.add(`rect-${col}`)
+    div.classList.add("rect");
+    div.classList.add(`rect-${col}`);
+    div.setAttribute("margin", 0);
+    div.style.height = `${height}px`;
     for(let row = 0; row < dimension; row++) {
         const childDiv = document.createElement("div");
+        childDiv.classList.add("square");
         childDiv.classList.add(`square-${row}-${col}`);
+        childDiv.style.width = `${width}px`;
+        childDiv.style.height = `${height}px`;
         div.appendChild(childDiv);
     }
 }
