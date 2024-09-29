0f214bb1d40d642ef34d877ca42fc8f8b0b9670e
parangat11 <parangataditya001@gmail.com>
2024-09-29 16:05:42 +0530

    Create an n * n grid using DOM inside the canvas.
    We now have our sketchpad, but logic to color it still remains.

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 let dimension = Number(prompt("Enter the dimension for canvas"));
+
+// Logic to ensure that the canvas dimension is a number between 10 and 100
+dimension = Math.max(16, dimension);
+dimension = Math.min(100, dimension);
 console.log(dimension);
+
+// Creating the canvas
+const container = document.querySelector(".container");
+
+for(let col = 0; col < dimension; col++) {
+    const div = document.createElement("div");
+    container.appendChild(div);
+    div.classList.add(`rect-${col}`)
+    for(let row = 0; row < dimension; row++) {
+        const childDiv = document.createElement("div");
+        childDiv.classList.add(`square-${row}-${col}`);
+        div.appendChild(childDiv);
+    }
+}
